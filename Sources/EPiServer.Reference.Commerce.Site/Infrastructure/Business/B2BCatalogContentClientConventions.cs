a2ad35b34428f28addaedb2423cf7a0ddbe99cd1
hung.dang <danghung1202@gmail.com>
2018-12-26 18:22:45 +0700

    Add Infrastructure folder

---
+++ Sources/EPiServer.Reference.Commerce.Site/Infrastructure/Business/B2BCatalogContentClientConventions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EPiServer.Find.ClientConventions;
+using EPiServer.Find.Commerce;
+using EPiServer.Reference.Commerce.Site.Features.Product.Models;
+using EPiServer.ServiceLocation;
+
+namespace EPiServer.Reference.Commerce.Site.Infrastructure.Business
+{
+    [ServiceConfiguration(ServiceType = typeof(CatalogContentClientConventions), Lifecycle = ServiceInstanceScope.Singleton)]
+    public class B2BCatalogContentClientConventions : CatalogContentClientConventions
+    {
+        protected override void ApplyNestedConventions(NestedConventions nestedConventions)
+        {
+            try
+            {
+                base.ApplyNestedConventions(nestedConventions);
+                nestedConventions.ForType<BaseProduct>().Add(p => p.ListingPrices);
+                nestedConventions.ForType<BaseProduct>().Add(p => p.OriginalPrices);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}
